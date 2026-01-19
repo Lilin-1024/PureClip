64f1aef3d2aa1aa5b7df16e8a08d6ce45cd1b006
Lilin <LingeonU@gmail.com>
2026-01-19 10:50:44 +0800

    缩放

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,8 @@ namespace PureClip
     public partial class Form1 : Form
     {
         private Rectangle _minCanvas;
+
+        public int _CanvasSize = 300;
         public Form1()
         {
             InitializeComponent();
@@ -16,7 +18,8 @@ namespace PureClip
             this.Bounds = Screen.PrimaryScreen.Bounds;
 
             var screen = Screen.PrimaryScreen.WorkingArea;
-            _minCanvas = new Rectangle((screen.Width - 300) / 2, (screen.Height - 300) / 2, 300, 300);
+
+            _minCanvas = new Rectangle((screen.Width - _CanvasSize) / 2, (screen.Height - _CanvasSize) / 2, _CanvasSize, _CanvasSize);
 
             Color ghostColor = Color.FromArgb(255, 1, 1, 1); 
             this.BackColor = ghostColor;
@@ -79,34 +82,41 @@ namespace PureClip
 
         private List<ClipItem> _items = new List<ClipItem>();
 
+        private float _canvasPadding = 20f;
+        private const float MIN_PADDING = 20f;
         private Rectangle GetCurrentCanvasBounds()
         {
-            int margin = 20;
+            Rectangle screen = Screen.PrimaryScreen.WorkingArea;
+            int margin = (int)_canvasPadding;
 
             if (_items.Count == 0)
             {
                 return _minCanvas;
             }
 
-            float minX = _items[0].X;
-            float minY = _items[0].Y;
-            float maxX = _items[0].X + _items[0].DisplayWidth;
-            float maxY = _items[0].Y + _items[0].DisplayHeight;
+            float minX = _items.Min(i => i.X);
+            float minY = _items.Min(i => i.Y);
+            float maxX = _items.Max(i => i.X + i.DisplayWidth);
+            float maxY = _items.Max(i => i.Y + i.DisplayHeight);
 
-            foreach (var item in _items)
-            {
-                if (item.X < minX) minX = item.X;
-                if (item.Y < minY) minY = item.Y;
-                if (item.X + item.DisplayWidth > maxX) maxX = item.X + item.DisplayWidth;
-                if (item.Y + item.DisplayHeight > maxY) maxY = item.Y + item.DisplayHeight;
-            }
+            int targetW = (int)(maxX - minX + margin * 2);
+            int targetH = (int)(maxY - minY + margin * 2);
 
-            return new Rectangle(
-                (int)minX - margin,
-                (int)minY - margin,
-                (int)(maxX - minX) + margin * 2,
-                (int)(maxY - minY) + margin * 2
-            );
+            if (targetW > screen.Width) targetW = screen.Width;
+            if (targetH > screen.Height) targetH = screen.Height;
+
+            int centerX = (int)((minX + maxX) / 2);
+            int centerY = (int)((minY + maxY) / 2);
+
+            int x = centerX - targetW / 2;
+            int y = centerY - targetH / 2;
+
+            if (x < screen.Left) x = screen.Left;
+            if (y < screen.Top) y = screen.Top;
+            if (x + targetW > screen.Right) x = screen.Right - targetW;
+            if (y + targetH > screen.Bottom) y = screen.Bottom - targetH;
+
+            return new Rectangle(x, y, targetW, targetH);
         }
         protected override void OnPaint(PaintEventArgs e)
         {
@@ -131,28 +141,42 @@ namespace PureClip
             foreach (var item in _items)
             {
                 g.DrawImage(item.ImageData, item.X, item.Y, item.DisplayWidth, item.DisplayHeight);
+                if (item == _selectedItem)
+                {
+                    using (Pen p = new Pen(Color.FromArgb(150, 100, 100, 255), 2))
+                    {
+                        g.DrawRectangle(p, item.X - 1, item.Y - 1, item.DisplayWidth + 2, item.DisplayHeight + 2);
+                    }
+                }
             }
         }
 
         private ClipItem _draggingItem = null;
+        private ClipItem _selectedItem = null;
         private Point _lastMousePos;
 
         protected override void OnMouseDown(MouseEventArgs e)
         {
+            _selectedItem = null;
+
             for (int i = _items.Count - 1; i >= 0; i--)
             {
                 if (_items[i].Contains(e.X, e.Y))
                 {
+                    _selectedItem = _items[i];
                     _draggingItem = _items[i];
                     _lastMousePos = Cursor.Position;
 
-                    var item = _items[i];
                     _items.RemoveAt(i);
-                    _items.Add(item);
+                    _items.Add(_selectedItem);
 
                     Invalidate();
                     return;
                 }
+            }
+            if (GetCurrentCanvasBounds().Contains(e.Location))
+            {
+                Invalidate();
             }
         }
 
@@ -160,18 +184,79 @@ namespace PureClip
         {
             if (_draggingItem != null && e.Button == MouseButtons.Left)
             {
-                Point currentPos = Cursor.Position;
-                _draggingItem.X += (currentPos.X - _lastMousePos.X);
-                _draggingItem.Y += (currentPos.Y - _lastMousePos.Y);
+                Point currentMousePos = Cursor.Position;
+                float dx = currentMousePos.X - _lastMousePos.X;
+                float dy = currentMousePos.Y - _lastMousePos.Y;
 
-                _lastMousePos = currentPos;
-                Invalidate(); // ÖØÐÂ»­Í¼
+                float nextX = _draggingItem.X + dx;
+                float nextY = _draggingItem.Y + dy;
+
+                Rectangle screen = Screen.PrimaryScreen.WorkingArea;
+
+                int wallMargin = 10;
+
+                if (nextX < screen.Left + wallMargin) nextX = screen.Left +wallMargin;
+                if (nextX + _draggingItem.DisplayWidth + wallMargin > screen.Right)
+                    nextX = screen.Right - _draggingItem.DisplayWidth - wallMargin;
+
+                if (nextY < screen.Top + wallMargin) nextY = screen.Top + wallMargin;
+
+                if (nextY + _draggingItem.DisplayHeight + wallMargin > screen.Bottom)
+                    nextY = screen.Bottom - _draggingItem.DisplayHeight - wallMargin;
+
+                _draggingItem.X = nextX;
+                _draggingItem.Y = nextY;
+
+                _lastMousePos = currentMousePos;
+
+                Invalidate();
             }
         }
 
         protected override void OnMouseUp(MouseEventArgs e)
         {
             _draggingItem = null;
+        }
+
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            float zoomFactor = e.Delta > 0 ? 1.1f : 0.9f;
+
+            if (_selectedItem != null)
+            {
+                float oldWidth = _selectedItem.DisplayWidth;
+                float oldHeight = _selectedItem.DisplayHeight;
+
+                _selectedItem.Scale *= zoomFactor;
+
+                if (_selectedItem.Scale < 0.01f) _selectedItem.Scale = 0.01f;
+
+                _selectedItem.X -= (_selectedItem.DisplayWidth - oldWidth) / 2f;
+                _selectedItem.Y -= (_selectedItem.DisplayHeight - oldHeight) / 2f;
+            }
+            else
+            {
+                float oldPadding = _canvasPadding;
+                _canvasPadding *= zoomFactor>1f? (zoomFactor + 0.1f): (zoomFactor - 0.1f);
+
+                if (_canvasPadding < MIN_PADDING) _canvasPadding = MIN_PADDING;
+
+                int oldW = _minCanvas.Width;
+                int oldH = _minCanvas.Height;
+
+                int newW = (int)(oldW * zoomFactor);
+                int newH = (int)(oldH * zoomFactor);
+
+                if (newW > _CanvasSize && newH > _CanvasSize)
+                {
+                    int dx = (newW - oldW) / 2;
+                    int dy = (newH - oldH) / 2;
+
+                    _minCanvas = new Rectangle(_minCanvas.X - dx, _minCanvas.Y - dy, newW, newH);
+                }
+            }
+
+            Invalidate();
         }
 
         protected override void OnKeyDown(KeyEventArgs e)
